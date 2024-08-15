6ea80800e370f8fa41aba788f1912920e7749a85
yuliavolos8 <147557438+yuliavolos8@users.noreply.github.com>
2024-08-15 19:07:50 +0200

    Enum

---
+++ src/enumParaTallas/Ropa.java
@@ -0,0 +1,35 @@
+package enumParaTallas;
+
+public class Ropa {
+	public enum Talla {
+		XS, S, M, L, XL
+	};
+
+	private String prenda;
+	private Talla talla;
+
+	public Ropa(String prenda, Talla t) {
+		this.prenda = prenda;
+		this.talla = t;
+	}
+
+	public String getPrenda() {
+		return prenda + " talla " + talla.toString();
+	}
+
+	public Talla getTalla() {
+		return talla;
+	}
+
+	public void setTalla(Talla t) {
+		this.talla = t;
+	}
+
+	public static void main(String args[]) {
+		Ropa ropa = new Ropa("vestido", Talla.S);
+		System.out.println(ropa.getPrenda());
+		ropa.setTalla(Talla.XL);
+		System.out.println(ropa.getPrenda());
+	}
+
+}
